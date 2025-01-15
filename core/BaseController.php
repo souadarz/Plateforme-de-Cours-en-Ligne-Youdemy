51b19155ebbbafc3225968919f6692a9064ef289
soaud arziki <arziki.souad@gmail.com>
2025-01-15 09:42:06 +0100

    structure du projet

---
+++ core/BaseController.php
@@ -0,0 +1,13 @@
+<?php 
+
+class BaseController
+{
+    // Render a view
+    public function render($view, $data = [])
+    {
+        
+        extract($data);
+        include __DIR__ . '/../app/views/' . $view . '.php';
+    }
+   
+}
